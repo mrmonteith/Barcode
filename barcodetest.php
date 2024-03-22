788b9e12559013049473e7f58d064fa8b52e9831
mrmonteith <michael_r_monteith@yahoo.com>
2024-03-22 05:39:37 -0400

    Barcode Test Initial Commit

---
+++ barcodetest.php
@@ -0,0 +1,8 @@
+<?php
+require 'vendor/autoload.php';
+
+// This will output the barcode as HTML output to display in the browser
+$generator = new Picqer\Barcode\BarcodeGeneratorHTML();
+
+echo $generator->getBarcode('081231723897', $generator::TYPE_CODE_128);
+echo "081231723897";
